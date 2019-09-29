d7ae40c30d4699e4375f5592dad44918ce9b43c2
shreejoy <pshreejoy15@gmail.com>
2019-09-29 16:03:37 +0530

    Doze: Nuke it for now

--- device.mk
+++ device.mk
@@ -417,7 +417,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
-
-# Xiaomeme doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
